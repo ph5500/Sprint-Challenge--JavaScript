37a4537f50ba8ffdb593b9815850e817966737ee
ph5500 <philfives@gmail.com>
2020-02-28 12:06:21 -0700

    completed request 1 and 2 on objects-array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -80,7 +80,14 @@ console.log(dinosaur_tyran.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+const obj = {
+  roar() {
+    console.log(obj);
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+}
+
+
 
 
 // ==== Arrays ====
@@ -104,7 +111,13 @@ const graduates = [
  
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities);
+
+
+for (let i = 0; i < graduates.length; i++) {
+  universities[i] = graduates[i].university
+}
+universities.sort();
+console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
  
@@ -113,6 +126,10 @@ The resulting contact information strings should have a space between the first 
  
 Log the result of your new array. */
 const contactInfo = [];
+
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo[i] = `${graduates[i].first_name} ${graduates[i].email}`
+}
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
@@ -142,7 +159,7 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
  
 */
-const displayNames = [];
+const displayNames = ["animal_name", "scientific_name"];
 console.log(displayNames);
 
 /* Request 2: .map()
